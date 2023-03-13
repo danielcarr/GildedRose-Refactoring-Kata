6c1d49f8c07ba94bed0832168373cc6af946477e
Daniel Carr <dcarr@oldmutual.com>
2023-03-13 16:38:53 +0200

    Add some tests

--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -2,10 +2,52 @@
 import XCTest
 
 class GildedRoseTests: XCTestCase {
-    func testFoo() throws {
+    func testNameDoesntChange() throws {
         let items = [Item(name: "foo", sellIn: 0, quality: 0)]
         let app = GildedRose(items: items)
         app.updateQuality()
-        XCTAssertEqual(app.items[0].name, "fixme")
+        XCTAssertEqual(app.items[0].name, "foo")
+    }
+
+    func testDailyUpdate() throws {
+        let startingSellIn = 10
+        let startingQuality = 5
+        let items = [Item(name: "foo", sellIn: startingSellIn, quality: startingQuality)]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+        let item = items.first!
+        XCTAssertLessThan(item.sellIn, startingSellIn)
+        XCTAssertLessThan(item.quality, startingQuality)
+    }
+
+    func testDegradationSpeedAfterSellBy() throws {
+        let name = "foo"
+        let startingQuality = 5
+        let items = [
+            Item(name: name, sellIn: 1, quality: startingQuality),
+            Item(name: name, sellIn: 0, quality: startingQuality)
+        ]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+
+        let fresh = items.first!
+        let expired = items.last!
+
+        let freshQualityDelta = startingQuality - fresh.quality
+        let expiredQualityDelta =  startingQuality - expired.quality
+
+        XCTAssertEqual(freshQualityDelta * 2, expiredQualityDelta)
+    }
+
+    func testQualityNeverNegative() throws {
+        let name = "foo"
+        let startingQuality = 0
+        let items = [
+            Item(name: name, sellIn: 0, quality: startingQuality)
+        ]
+        let app = GildedRose(items: items)
+        app.updateQuality()
+
+        XCTAssertGreaterThanOrEqual(items.first!.quality, 0)
     }
 }
